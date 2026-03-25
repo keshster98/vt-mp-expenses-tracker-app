3c974aadb20c8aa580b1fadd51080800719bc1c7
Keshen <njkeshen98@gmail.com>
2026-03-25 17:32:35 +0800

    Modify date_formatter to allow for AM/PM formatting

--- utils/date_formatter.js
+++ utils/date_formatter.js
@@ -3,5 +3,13 @@ export const formatDate = (dateString) => {
 
   const date = new Date(dateString);
 
-  return date.toLocaleDateString() + " " + date.toLocaleTimeString();
+  return (
+    date.toLocaleDateString() +
+    " " +
+    date.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    })
+  );
 };
